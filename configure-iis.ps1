d4ce873234df855aa3fd09294d61ada55140c1a8
yuecharlielee <35199397+yuecharlielee@users.noreply.github.com>
2020-11-11 15:16:24 +0800

    Update configure-iis.ps1

--- configure-iis.ps1
+++ configure-iis.ps1
@@ -1,7 +1,8 @@
+
 # Install IIS.
 dism /online /enable-feature /featurename:IIS-WebServerRole
 
 # Set the home page.
 Set-Content `
   -Path "C:\\inetpub\\wwwroot\\Default.htm" `
-  -Value "<html><body>https://app.powerbi.com/view?r=eyJrIjoiYzg3ODQyMzQtMDBmZi00YWQzLWIzYmEtZTRkNGM0NmJmYzc5IiwidCI6IjI4ZDBmYTc1LWY5ZjktNDAyNC05MzM3LTQ4NWQ0NmU3NTI1NyIsImMiOjEwfQ%3D%3D&pageName=ReportSection</body></html>"
+  -Value "<html><body><iframe width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiYzg3ODQyMzQtMDBmZi00YWQzLWIzYmEtZTRkNGM0NmJmYzc5IiwidCI6IjI4ZDBmYTc1LWY5ZjktNDAyNC05MzM3LTQ4NWQ0NmU3NTI1NyIsImMiOjEwfQ%3D%3D&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe></body></html>"
